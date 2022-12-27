0298af9d91b3caa2b274b2495614004ee1fc3bee
Heroku-v2rays <94675428+yuiosa@users.noreply.github.com>
2022-12-27 23:18:08 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: `/server/`,
-    bare: `/https://hideois.952768.xyz/`,
+    bare: `https://hideois.952768.xyz`,
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/server/server.handler.js',
